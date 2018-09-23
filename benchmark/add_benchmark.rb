c5ec1f01e0dad1f216ee406dec55cc546d2e1949
yuihyama <yuihyama@gmail.com>
2018-09-23 11:33:27 +0900

    Rename addition -> add

---
+++ benchmark/add_benchmark.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'benchmark'
+require './lib/add'
+
+Benchmark.bmbm do |x|
+  x.report('1.add(2)') { 1.add(2) }
+  x.report('[1, 2, 3].add(2)') { [1, 2, 3].add(2) }
+
+  x.report('300000.times { 1.add(2) }') {
+    300000.times { 1.add(2) }
+  }
+  x.report('300000.times { [1, 2, 3].add(2) }') {
+    300000.times { [1, 2, 3].add(2) }
+  }
+end
