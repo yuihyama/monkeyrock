9f119a318adc4fb696397b7b8f9785d0f0623990
yuihyama <yuihyama@gmail.com>
2018-09-23 12:41:53 +0900

    Add subt_test.rb

---
+++ test/subt_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require './lib/subt'
+
+class SubtTest < Minitest::Test
+  def test_subt
+    assert_equal 1, 2.subt(1)
+    assert_equal 0, 0.subt(0)
+    assert_equal(-1, 0.subt(1))
+    assert_equal 1, 1.subt(0)
+    assert_equal 1, 0.subt(-1)
+    assert_equal(-1, -1.subt(0))
+    assert_equal 0.0, -1.subt(-1.0)
+    assert_equal 0.0, (-1.0).subt(-1)
+    assert_equal 0.0, -1.0.subt(-1)
+    assert_equal(-0, (-1.0).subt(-1.0))
+    assert_equal 0.0, -1.0.subt(-1.0)
+    assert_equal 1.0, (2.1).subt(1.1)
+    assert_equal(-1.0, 1.1.subt(2.1))
+    assert_equal [0, 1, 2], [1, 2, 3].subt(1)
+    assert_equal [-1, 0.0, 1], [1, 2.0, 3].subt(2)
+    assert_equal [2, 3, 4], [1, 2, 3].subt(-1)
+
+    assert_raises(NoMethodError) { 'a'.subt(1) }
+    assert_raises(NoMethodError) { 'a string'.subt(1) }
+    assert_raises(TypeError) { (1.0).subt('a') }
+    assert_raises(TypeError) { 1.0.subt('a') }
+
+    assert_output("1\n") { p 2.subt(1) }
+    assert_output("0\n") { p (-1.subt(-1)) }
+    assert_output("0.0\n") { p (-1.subt(-1.0)) }
+    assert_output("1.0\n") { p (-1.0.subt(-2.0)) }
+    assert_output("[0, 1, 2]\n") { p [1, 2, 3].subt(1) }
+    assert_output("0.0\n1.0\n2.0\n") { puts [1, 2, 3].subt(1.0) }
+    assert_output("[-1, 0.0, 1]\n") { print [1, 2.0, 3].subt(2), "\n" }
+  end
+end
