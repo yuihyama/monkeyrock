0f9ecac907cb6f313f46ca0146cea82a0fbe2120
yuihyama <yuihyama@gmail.com>
2018-09-25 19:45:48 +0900

    Add add_test.rb

--- test/add_test.rb
+++ test/add_test.rb
@@ -16,6 +16,8 @@ class AddTest < Minitest::Test
     assert_equal(-2.0, -1.0.add(-1.0))
     assert_equal 2.2, (1.1).add(1.1)
     assert_equal 2.2, 1.1.add(1.1)
+    # Expected, Actual, (5/3)
+    assert_equal Rational(5, 3), Rational(2, 3).add(1)
     assert_equal [3, 4, 5], [1, 2, 3].add(2)
     assert_equal [3, 4.0, 5], [1, 2.0, 3].add(2)
     assert_equal [0, 1, 2], [1, 2, 3].add(-1)
