820f18f078cf120a1fc45e6a791db233d8d9c9b0
yuihyama <yuihyama@gmail.com>
2018-09-26 12:00:20 +0900

    Clean add_benchmark.rb

--- benchmark/add_benchmark.rb
+++ benchmark/add_benchmark.rb
@@ -9,16 +9,16 @@ Benchmark.bmbm do |x|
   x.report('[1, 2, 3].add(2)') { [1, 2, 3].add(2) }
   x.report('(1..3).add(2)') { (1..3).add(2) }
 
-  x.report('300000.times { 1.add(2) }') do
+  x.report('300_000.times { 1.add(2) }') do
     300_000.times { 1.add(2) }
   end
-  x.report('300000.times { Rational(2, 3).add(1) }') do
+  x.report('300_000.times { Rational(2, 3).add(1) }') do
     300_000.times { Rational(2, 3).add(1) }
   end
-  x.report('300000.times { [1, 2, 3].add(2) }') do
+  x.report('300_000.times { [1, 2, 3].add(2) }') do
     300_000.times { [1, 2, 3].add(2) }
   end
-  x.report('300000.times { (1..3).add(2) }') do
+  x.report('300_000.times { (1..3).add(2) }') do
     300_000.times { (1..3).add(2) }
   end
 end
