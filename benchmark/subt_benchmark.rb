18d84dc3295a0b9822ce4465b8a6a8accd0c2305
yuihyama <yuihyama@gmail.com>
2018-09-26 11:59:52 +0900

    Update subt_benchmark.rb

--- benchmark/subt_benchmark.rb
+++ benchmark/subt_benchmark.rb
@@ -7,6 +7,7 @@ Benchmark.bmbm do |x|
   x.report('2.subt(1)') { 2.subt(1) }
   x.report('Rational(2, 3).subt(1)') { Rational(2, 3).subt(1) }
   x.report('[1, 2, 3].subt(1)') { [1, 2, 3].subt(1) }
+  x.report('(1..3).subt(1)') { (1..3).subt(1) }
 
   x.report('300_000.times { 2.subt(1) }') do
     300_000.times { 2.subt(1) }
@@ -16,5 +17,8 @@ Benchmark.bmbm do |x|
   end
   x.report('300_000.times { [1, 2, 3].subt(1) }') do
     300_000.times { [1, 2, 3].subt(1) }
+  end
+  x.report('300_000.times { (1..3).subt(1) }') do
+    300_000.times { (1..3).subt(1) }
   end
 end
