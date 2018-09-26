7a198c31945df185e2acd48b28cdd7de7fc1aa7b
yuihyama <yuihyama@gmail.com>
2018-09-26 11:57:02 +0900

    Update subt_benchmark.rb

--- benchmark/subt_benchmark.rb
+++ benchmark/subt_benchmark.rb
@@ -5,12 +5,16 @@ require './lib/subt'
 
 Benchmark.bmbm do |x|
   x.report('2.subt(1)') { 2.subt(1) }
+  x.report('Rational(2, 3).subt(1)') { Rational(2, 3).subt(1) }
   x.report('[1, 2, 3].subt(1)') { [1, 2, 3].subt(1) }
 
-  x.report('300000.times { 2.subt(1) }') do
+  x.report('300_000.times { 2.subt(1) }') do
     300_000.times { 2.subt(1) }
   end
-  x.report('300000.times { [1, 2, 3].subt(1) }') do
+  x.report('300_000.times { Rational(2, 3).subt(1) }') do
+    300_000.times { Rational(2, 3).subt(1) }
+  end
+  x.report('300_000.times { [1, 2, 3].subt(1) }') do
     300_000.times { [1, 2, 3].subt(1) }
   end
 end
