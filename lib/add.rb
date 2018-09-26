e74202682826380711df63d1cf4f645908647a8c
yuihyama <yuihyama@gmail.com>
2018-09-26 13:26:45 +0900

    Update add.rb

--- lib/add.rb
+++ lib/add.rb
@@ -1,10 +1,13 @@
 module Addition
   def add(x)
-    if instance_of?(Integer) || instance_of?(Float) || instance_of?(Rational)
-      send(:+, x)
-    elsif instance_of?(Array) || instance_of?(Range)
-      pr = proc { |n| n + x }
-      map(&pr)
+    [Integer, Float, Rational].each do |klass|
+      return send(:+, x) if instance_of?(klass)
+    end
+    [Array, Range].each do |klass|
+      if instance_of?(klass)
+        pr = proc { |n| n + x }
+        return map(&pr)
+      end
     end
   end
 end
@@ -20,3 +23,7 @@ end
 class Range
   include Addition
 end
+
+# p 1.add(2)
+# p [1, 2, 3].add(1)
+# p (-1.0).add(-1)
