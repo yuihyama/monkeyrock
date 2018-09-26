38227f4e0cd6daab61c4f6986b5c64456f30b1af
yuihyama <yuihyama@gmail.com>
2018-09-26 14:35:09 +0900

    Add multiply.rb

---
+++ lib/multiply.rb
@@ -0,0 +1,25 @@
+module Multiplication
+  def multiply(x)
+    [Integer, Float, Rational].each do |klass|
+      return send(:*, x) if instance_of?(klass)
+    end
+    [Array, Range].each do |klass|
+      if instance_of?(klass)
+        pr = proc { |n| n * x }
+        return map(&pr)
+      end
+    end
+  end
+end
+
+class Numeric
+  include Multiplication
+end
+
+class Array
+  include Multiplication
+end
+
+class Range
+  include Multiplication
+end
