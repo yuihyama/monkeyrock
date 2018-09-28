5d22ed55f0cc7d1f53ada8a44372353e7f383be0
yuihyama <yuihyama@gmail.com>
2018-09-28 09:58:35 +0900

    Update multiply.rb

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,8 +1,6 @@
 module Multiplication
   def multiply(x)
-    return if instance_of?(Hash)
-
-    [Integer, Float, Rational].each do |klass|
+    [Integer, Float, Rational, Complex].each do |klass|
       return send(:*, x) if instance_of?(klass)
     end
 
